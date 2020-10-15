e10fb590a9a0d54b907d0fa8bd923987ece7fa19
swiercm <60585983+swiercm@users.noreply.github.com>
2020-10-15 19:44:19 +0100

    updated to LCA in BST

--- LCA.py
+++ LCA.py
@@ -4,55 +4,38 @@ class Node:
         self.left = None
         self.right = None
 
-def findPath(root, path, k):
-
-    if root is None:
-        return False
-
-    path.append(root.key)
-
-    if root.key == k :
-        return True
-
-    if ((root.left != None and findPath(root.left, path, k)) or (root.right != None and findPath(root.right, path, k))):
-        return True
-
-    path.pop()
-    return False
 
 def findLCA(root, n1, n2):
 
-    path1 = []
-    path2 = []
+    if root is None:
+        return None
 
-    if(not findPath(root, path1, n1) or not findPath(root, path2, n2)):
-        return -1
+    if (n1 > root.key and n2 > root.key):
+        return findLCA(root.right, n1, n2)
 
-    i=0
-    while(i<len(path1) and 1<len(path2)):
-        if path1[i] != path2[i]:
-            break
-        i += 1
-    return path1[i-1]
+    if (n1 < root.key and n2 < root.key):
+        return findLCA(root.left, n1, n2)
+
+    return root.key
 
 
 
-#           1
+#           4
 #         /   \
-#        2      3
+#        3      6
 #       / \    / \
-#      4   5  6   7
+#      1   2  5   7
 
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4) 
-root.left.right = Node(5) 
-root.right.left = Node(6) 
+root = Node(4)
+root.left = Node(3)
+root.right = Node(6)
+root.left.left = Node(1) 
+root.left.right = Node(2) 
+root.right.left = Node(5) 
 root.right.right = Node(7)
 
 
 print ("LCA(4, 5) = %d" %findLCA(root, 4, 5))
-print ("LCA(4, 6) = %d" %findLCA(root, 4, 6))
-print ("LCA(3, 4) = %d" %findLCA(root, 3, 4))
-print ("LCA(2, 5) = %d" %findLCA(root, 2, 4))
+print ("LCA(1, 2) = %d" %findLCA(root, 1, 2))
+print ("LCA(3, 2) = %d" %findLCA(root, 3, 2))
+print ("LCA(5, 7) = %d" %findLCA(root, 5, 7))
