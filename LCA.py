9f25a141309c807854244e6d58323a271b9b8f69
swiercm <60585983+swiercm@users.noreply.github.com>
2020-10-18 21:20:17 +0100

    Added clause if key is not in tree (+ fixed tree layout)

--- LCA.py
+++ LCA.py
@@ -4,8 +4,20 @@ class Node:
         self.left = None
         self.right = None
 
+    def find(self, x):
+        if self.key == x:
+            return True
+        elif x < self.key and self.left:
+            return self.left.find(x)
+        elif x > self.key and self.right:
+            return self.right.find(x)
+        return False
+
 
 def findLCA(root, n1, n2):
+
+    if  not root.find(n1) or not root.find(n2):
+        return None
 
     if root is None:
         return None
@@ -22,18 +34,17 @@ def findLCA(root, n1, n2):
 
 #           4
 #         /   \
-#        3      6
+#        2      6
 #       / \    / \
-#      1   2  5   7
+#      1   3  5   7
 
 root = Node(4)
-root.left = Node(3)
+root.left = Node(2)
 root.right = Node(6)
 root.left.left = Node(1) 
-root.left.right = Node(2) 
+root.left.right = Node(3) 
 root.right.left = Node(5) 
 root.right.right = Node(7)
-
 
 print ("LCA(4, 5) = %d" %findLCA(root, 4, 5))
 print ("LCA(1, 2) = %d" %findLCA(root, 1, 2))
