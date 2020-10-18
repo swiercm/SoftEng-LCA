9f4cb1ad3c45172bf4233f3095139d7d43bf7e73
swiercm <60585983+swiercm@users.noreply.github.com>
2020-10-18 22:20:50 +0100

    Added clause if not in tree (+ fixed tree layout)

--- lca.go
+++ lca.go
@@ -8,7 +8,22 @@ type Node struct {
 	right *Node
 }
 
+func find(root *Node, x int) bool {
+	if root.val == x {
+		return true
+	} else if x < root.val && root.left != nil {
+		return find(root.left, x)
+	} else if x > root.val && root.right != nil {
+		return find(root.right, x)
+	}
+	return false
+}
+
 func findLCA(root *Node, n1 int, n2 int) int {
+	if !find(root, n1) || !find(root, n2) {
+		return -1
+	}
+
 	if root == nil {
 		return -1
 	}
@@ -27,15 +42,15 @@ func findLCA(root *Node, n1 int, n2 int) int {
 func main() {
 	//            4
 	//         /    \
-	//        3      6
+	//        2      6
 	//       / \    / \
-	//      1   2  5   7
+	//      1   3  5   7
 
 	root := Node{4, nil, nil}
-	root.left = &Node{3, nil, nil}
+	root.left = &Node{2, nil, nil}
 	root.right = &Node{6, nil, nil}
 	root.left.left = &Node{1, nil, nil}
-	root.left.right = &Node{2, nil, nil}
+	root.left.right = &Node{3, nil, nil}
 	root.right.left = &Node{5, nil, nil}
 	root.right.right = &Node{7, nil, nil}
 
